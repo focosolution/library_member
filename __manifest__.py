6199eedb876ffbac63bb3cd04c3633d916bcf525
image4ever <paulofreire@paulofreire.biz>
2021-06-07 16:31:56 -0300

    primeiro commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+'name': 'Library Members and Borrowing',
+'description': 'Use library cards for people to borrow books.',
+'author': 'Daniel Reis',
+'depends': [
+    'library_app',
+],
+'application': False,
+}
+
---
+++ models/library_book.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+
+class Book(models.Model):
+    _inherit = 'library.book'
+    is_available = fields.Boolean('Is Available?')
+
