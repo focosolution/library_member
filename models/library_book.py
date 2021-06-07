b16c64612d606145f6caba0110affe394f53a6b5
image4ever <paulofreire@paulofreire.biz>
2021-06-07 19:08:23 -0300

    mudanças no form com herança

--- models/library_book.py
+++ models/library_book.py
@@ -5,3 +5,7 @@ class Book(models.Model):
     _inherit = 'library.book'
     is_available = fields.Boolean('Is Available?')
 
+    # Os campos abaixo já existiam e foram modificados. Para isso foi utilizado o mesmo tipo e adicionando 
+    # num caso o help e no outro a indexação. O restante permanece inalterado.
+    isbn = fields.Char(help="Use a valid ISBN-13 or ISBN-10.")
+    publisher_id = fields.Many2one(index=True)
