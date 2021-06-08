a0a0a1495fbbe940f77a9d4f823e23962dfa8b57
image4ever <paulofreire@paulofreire.biz>
2021-06-08 16:07:51 -0300

    Adicionando library member

--- models/library_member.py
+++ models/library_member.py
@@ -4,7 +4,6 @@ from odoo import fields, models
 class Member(models.Model):
     _name = 'library.member'
     _description = 'Library Member'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     card_number = fields.Char()
     partner_id = fields.Many2one(
